ed70818ab9cb145135fbaefdc8e897305604872e
nelys Mollocondo <nelys.mp@gmail.com>
2017-09-04 15:37:51 -0500

    Realización de PeriodoService y Periodo Implementación

---
+++ src/main/java/com/swaaad/service/impl/PeriodoServiceImpl.java
@@ -0,0 +1,35 @@
+package com.swaaad.service.impl;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.swaaad.dao.PeriodoDao;
+import com.swaaad.model.Periodo;
+import com.swaaad.service.PeriodoService;
+
+public class PeriodoServiceImpl implements PeriodoService {
+	@Autowired
+	PeriodoDao objPeriodoDao;
+
+	@Override
+	public void addPeriodo(Periodo periodo) throws Exception {
+		objPeriodoDao.addPeriodo(periodo);
+	}
+
+	@Override
+	public Periodo getPeriodoById(int idPeriodo) throws Exception {
+
+		return objPeriodoDao.getPeriodoById(idPeriodo);
+	}
+
+	@Override
+	public void updatePeriodo(Periodo periodo) throws Exception {
+		objPeriodoDao.updatePeriodo(periodo);
+	}
+
+	@Override
+	public void deletePeriodo(int idPeriodo) throws Exception {
+		objPeriodoDao.deletePeriodo(idPeriodo);
+
+	}
+
+}
