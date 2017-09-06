a970d5edaf15a99cd39793d450e2a6d1a46585df
nelys Mollocondo <nelys.mp@gmail.com>
2017-09-06 11:51:35 -0500

    Actualización de AppController

--- src/main/java/com/swaaad/controller/AppController.java
+++ src/main/java/com/swaaad/controller/AppController.java
@@ -1,30 +1,17 @@
 package com.swaaad.controller;
 
-import java.io.IOException;
-import java.util.List;
-
-import javax.servlet.http.HttpServletRequest;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.servlet.ModelAndView;
-
-import com.swaaad.model.Alumno;
-import com.swaaad.service.ActividadPedagogicaService;
-import com.swaaad.service.AlumnosService;
 
 @Controller
 @RequestMapping("/")
 public class AppController {
 	private static final Logger logger = LoggerFactory.getLogger(AlumnoController.class);
-	@Autowired
-	AlumnosService objAlumnoService;
+	
 
 	@RequestMapping(value = { "/home" }, method = RequestMethod.GET)
 	public String homePage(ModelMap model) {
@@ -41,89 +28,11 @@ public class AppController {
 		return "cursos";
 	}
 
-	/////// nelys
-	/*@RequestMapping(value = { "/alumnos" }, method = RequestMethod.GET)
-	public ModelAndView alumnosPage(ModelAndView model) throws Exception {
-
-		logger.info("alumnosPage");
-		//
-		List<Alumno> ListarAlumno = null;
-
-		ListarAlumno = objAlumnoService.getAllAlumnos();
-
-		Alumno alumno = new Alumno();
-
-		model.addObject("alumno", alumno);
-		model.addObject("listAlumnos", ListarAlumno);
-
-		model.setViewName("alumnos");
-
-		return model;
-	}
-
-	@RequestMapping(value = "/saveAlumno", method = RequestMethod.POST)
-	public ModelAndView saveAlumno(@ModelAttribute Alumno alumno) throws Exception {
-
-		logger.info("saveAlumno");
-
-		try {
-			if (alumno.getIdAlumno() == 0) {
-				objAlumnoService.addAlumno(alumno);
-			} else {
-				objAlumnoService.updateAlumno(alumno);
-			}
-
-		} catch (Exception e) {
-			e.getStackTrace();
-		}
-		return new ModelAndView("redirect:/alumnos");
-	}
-
-	@RequestMapping(value = "/newAlumno", method = RequestMethod.GET)
-	public ModelAndView newAlumno(ModelAndView model) throws Exception {
-		logger.info("newAlumno");
-		Alumno alumno = new Alumno();
-		model.addObject("alumno", alumno);
-		model.setViewName("form-alumno");
-		return model;
-	}
-
-	@RequestMapping(value = "/editAlumno", method = RequestMethod.GET)
-	public ModelAndView editContact(HttpServletRequest request) throws Exception {
-		
-		int alumnoId = Integer.parseInt(request.getParameter("id"));
-		logger.info("editAlumno "+alumnoId);
-		Alumno alumno = null;
-		try {
-			alumno = objAlumnoService.getAlumnoById(alumnoId);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		ModelAndView model = new ModelAndView("form-alumno");
-		model.addObject("alumno", alumno);
-
-		return model;
-	}
-
-	@RequestMapping(value = "/deleteAlumno", method = RequestMethod.GET)
-	public ModelAndView deleteAlumno(HttpServletRequest request) throws Exception {
-		int alumnoId = Integer.parseInt(request.getParameter("id"));
-		logger.info("deleteAlumno " + alumnoId);
-		 try {
-		objAlumnoService.deleteAlumno(alumnoId);
-		 } catch (Exception e) {
-		// // TODO Auto-generated catch block
-		 e.printStackTrace();
-		 }
-		//
-		return new ModelAndView("redirect:/alumnos");
-	}*/
-
-	// fin nelys
-	@RequestMapping(value = { "/evaluaciones" }, method = RequestMethod.GET)
-	public String evaluacionesPage(ModelMap model) {
-		return "evaluaciones";
-	}
+	
+//	@RequestMapping(value = { "/evaluaciones" }, method = RequestMethod.GET)
+//	public String evaluacionesPage(ModelMap model) {
+//		return "evaluaciones";
+//	}
 
 	@RequestMapping(value = { "/asistencias" }, method = RequestMethod.GET)
 	public String asistenciasPage(ModelMap model) {
