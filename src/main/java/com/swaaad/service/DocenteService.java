e524e05af19c40596097802c43299e9193b318a7
nelys Mollocondo <nelys.mp@gmail.com>
2017-09-06 11:53:09 -0500

    Actualizacion de DocenteService

--- src/main/java/com/swaaad/service/DocenteService.java
+++ src/main/java/com/swaaad/service/DocenteService.java
@@ -1,7 +1,10 @@
 package com.swaaad.service;
 
+import org.springframework.stereotype.Service;
+
 import com.swaaad.model.Docente;
 
+@Service
 public interface DocenteService {
 
 	void addDocente(Docente docente) throws Exception;
