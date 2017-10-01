aa32ac81379b22b359732230060960bd4a29b271
christianvilca <christian.vilcaq@gmail.com>
2017-09-30 23:47:59 -0500

    Actualizando Formula

---
+++ src/main/java/com/swaaad/model/Formula1.java
@@ -0,0 +1,41 @@
+package com.swaaad.model;
+
+import java.io.Serializable;
+import javax.persistence.*;
+
+
+/**
+ * The persistent class for the formula_1 database table.
+ * 
+ */
+@Entity
+@Table(name="formula_1")
+@NamedQuery(name="Formula1.findAll", query="SELECT f FROM Formula1 f")
+public class Formula1 implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@Column(name="ID_EVALUACION")
+	private int idEvaluacion;
+
+	private String sistema;
+
+	public Formula1() {
+	}
+
+	public int getIdEvaluacion() {
+		return this.idEvaluacion;
+	}
+
+	public void setIdEvaluacion(int idEvaluacion) {
+		this.idEvaluacion = idEvaluacion;
+	}
+
+	public String getSistema() {
+		return this.sistema;
+	}
+
+	public void setSistema(String sistema) {
+		this.sistema = sistema;
+	}
+
+}
